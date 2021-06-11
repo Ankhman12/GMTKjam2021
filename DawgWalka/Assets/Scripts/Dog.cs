f5b4b643cacf83c3f6d9e42e5f03eeb4ec86bd42
Mitchell Dunning <ankhman12@gmail.com>
2021-06-11 17:11:20 -0400

    Added scripts for player control

---
+++ DawgWalka/Assets/Scripts/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    public Vector2 movement;
+    public float moveSpeed;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.y = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
