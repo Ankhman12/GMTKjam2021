eefee8994d1f385f35a9c07a29a97d61a77d1d6c
Will Ozeas <willatme@me.com>
2021-06-12 00:34:12 -0700

    Added in Skateboard SFX

--- DawgWalka/Assets/Scripts/AudioManager.cs
+++ DawgWalka/Assets/Scripts/AudioManager.cs
@@ -2,20 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GroundMaterial { Ground, Rail, Air }
+
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager i;
+
+    private const float MAX_SPEED = 5.1f;
+
+    private static FMOD.Studio.EventInstance skateboardNoise;
     
+    void Awake()
+    {
+        if(i != null)
+            GameObject.Destroy(i);
+        else
+            i = this;
+
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        skateboardNoise = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Skateboard/Skateboard");
+        StartSkateboardNoise();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float speedPercent = PlayerMovement.currentSpeed/MAX_SPEED; //1 is the speed after which the sfx doesn't change
+        Debug.Log(speedPercent);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Speed", speedPercent);
+    }
+
+    public void StartSkateboardNoise() {
+        skateboardNoise.start();
+        skateboardNoise.release();
+    }
+
+    public void StopSkateboardNoise() {
+        skateboardNoise.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void SkateboardJump() {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Skateboard/Jump");
+        skateboardNoise.setParameterByName("Ground Type", 2);
+    }
+
+    public void SkateboardLand(GroundMaterial ground) {
+        int groundParam = 0;
+        switch(ground) {
+            case GroundMaterial.Ground:
+                groundParam = 0;
+                break;
+            case GroundMaterial.Rail:
+                groundParam = 1;
+                break;
+            case GroundMaterial.Air:
+                groundParam = 2;
+                break;
+        }
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Skateboard/Land");
+        skateboardNoise.setParameterByName("Ground Type", groundParam);
     }
 
 }
--- DawgWalka/Assets/Scripts/PlayerMovement.cs
+++ DawgWalka/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,18 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     Rigidbody2D rb;
 
+    //these variables calculate the real speed of the player, for SFX
+    [System.NonSerialized]
+    public static float currentSpeed; 
+    Vector3 PrevPos; 
+    Vector3 NewPos; 
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
+        PrevPos = transform.position;
+        NewPos = transform.position;
     }
 
     // Update is called once per frame
@@ -24,5 +31,10 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+
+        //updating speed for SFX
+        NewPos = transform.position;  // each frame track the new position
+        currentSpeed = ((NewPos - PrevPos) / Time.fixedDeltaTime).magnitude;  // velocity = dist/time
+        PrevPos = NewPos;  // update position for next frame calculation
     }
 }
