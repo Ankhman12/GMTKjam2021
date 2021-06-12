7c9656a992600eb819be1028651fe27327977187
Will Ozeas <willatme@me.com>
2021-06-12 13:33:49 -0700

    Added Music & Basic Main Menu
    Also imported Text-mesh pro

--- DawgWalka/Assets/Scripts/AudioManager.cs
+++ DawgWalka/Assets/Scripts/AudioManager.cs
@@ -11,11 +11,15 @@ public class AudioManager : MonoBehaviour
     private const float MAX_SPEED = 5.1f;
 
     private static FMOD.Studio.EventInstance skateboardNoise;
+
+    private static FMOD.Studio.EventInstance music;
+
+    private static FMOD.Studio.EventInstance jingle;
     
     void Awake()
     {
         if(i != null)
-            GameObject.Destroy(i);
+            GameObject.Destroy(this);
         else
             i = this;
 
@@ -26,7 +30,8 @@ public class AudioManager : MonoBehaviour
     void Start()
     {
         skateboardNoise = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Skateboard/Skateboard");
-        StartSkateboardNoise();
+        music = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Level Music");
+        jingle = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Intro Jingle");
     }
 
     // Update is called once per frame
@@ -35,6 +40,38 @@ public class AudioManager : MonoBehaviour
         float speedPercent = PlayerMovement.currentSpeed/MAX_SPEED; //1 is the speed after which the sfx doesn't change
         Debug.Log(speedPercent);
         FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Speed", speedPercent);
+    }
+
+    public void StartGame() {
+        StopJingle();
+        StartSkateboardNoise();
+        StartMusic();
+    }
+
+    public void StartJingle() {
+        jingle.start();
+    }
+
+    public void StopJingle() {
+        jingle.release();
+        jingle.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void StartMusic() {
+        music.start();
+        music.release();
+    }
+
+    public void StopMusic() {
+        music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void OnPause() {
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Paused", 1);
+    }
+
+    public void OnUnpause() {
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Paused", 0);
     }
 
     public void StartSkateboardNoise() {
@@ -67,5 +104,6 @@ public class AudioManager : MonoBehaviour
         FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Skateboard/Land");
         skateboardNoise.setParameterByName("Ground Type", groundParam);
     }
+
 
 }
---
+++ DawgWalka/Assets/Scripts/Menu Scripts/MenuFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctions : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.i.StartJingle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() {
+        AudioManager.i.StartGame();
+        SceneManager.LoadScene("ProgrammingScene", LoadSceneMode.Single);
+    }
+
+    public void Quit() {
+        AudioManager.i.StartJingle();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
